0ffc3edd6e22d4083bddf4bb6adce845d832513a
ethanmoody27 <ethanmoody27@gmail.com>
2023-11-02 11:37:12 -0400

    Added proper error handling

--- server.js
+++ server.js
@@ -6,54 +6,82 @@ const prisma = require('./prisma/schema.prisma');
 
 app.use(express.json());
 
-app.get('/authors', async (req, res) => {
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ error: 'Internal server error' });
+});
+
+// Authors routes
+app.get('/authors', async (req, res, next) => {
+  try {
     const authors = await prisma.author.findMany();
     res.json(authors);
+  } catch (error) {
+    next(error);
+  }
 });
 
-app.post('/authors', async (req, res) => {
+app.post('/authors', async (req, res, next) => {
+  try {
     const { name } = req.body;
-    const author = await prisma.author.create({
-        data: { name }
-    });
+    const author = await prisma.author.create({ data: { name } });
     res.json(author);
+  } catch (error) {
+    next(error);
+  }
 });
 
-app.get('/authors/:id', async (req, res) => {
+app.get('/authors/:id', async (req, res, next) => {
+  try {
     const { id } = req.params;
     const author = await prisma.author.findUnique({ where: { id: parseInt(id) } });
     if (!author) {
-        res.status(404).json({ message: 'Author not found' });
+      res.status(404).json({ message: 'Author not found' });
     } else {
-        res.json(author);
+      res.json(author);
     }
+  } catch (error) {
+    next(error);
+  }
 });
 
-app.put('/authors/:id', async (req, res) => {
+app.put('/authors/:id', async (req, res, next) => {
+  try {
     const { id } = req.params;
     const { name } = req.body;
     const author = await prisma.author.update({
-        where: { id: parseInt(id) },
-        data: { name },
+      where: { id: parseInt(id) },
+      data: { name },
     });
     res.json(author);
+  } catch (error) {
+    next(error);
+  }
 });
 
-app.delete('/authors/:id', async (req, res) => {
+app.delete('/authors/:id', async (req, res, next) => {
+  try {
     const { id } = req.params;
     await prisma.author.delete({ where: { id: parseInt(id) } });
     res.json({ message: 'Author deleted' });
+  } catch (error) {
+    next(error);
+  }
 });
 
-app.get('/authors/:id/books', async (req, res) => {
+app.get('/authors/:id/books', async (req, res, next) => {
+  try {
     const { id } = req.params;
-    const books = await prisma.book.findMany({
-        where: { authorId: parseInt(id) },
-    });
+    const books = await prisma.book.findMany({ where: { authorId: parseInt(id) } });
     res.json(books);
+  } catch (error) {
+    next(error);
+  }
 });
 
-app.post('/authors/:id/books', async (req, res) => {
+app.post('/authors/:id/books', async (req, res, next) => {
+  try {
     const { id } = req.params;
     const { title } = req.body;
     const book = await prisma.book.create({
@@ -63,39 +91,59 @@ app.post('/authors/:id/books', async (req, res) => {
       },
     });
     res.json(book);
-  });
+  } catch (error) {
+    next(error);
+  }
+});
 
-  app.get('/books', async (req, res) => {
-      const books = await prisma.book.findMany();
-      res.json(books);
-  });
+// Books routes
+app.get('/books', async (req, res, next) => {
+  try {
+    const books = await prisma.book.findMany();
+    res.json(books);
+  } catch (error) {
+    next(error);
+  }
+});
 
-  app.get('/books/:id', async (req, res) => {
+app.get('/books/:id', async (req, res, next) => {
+  try {
     const { id } = req.params;
     const book = await prisma.book.findUnique({ where: { id: parseInt(id) } });
     if (!book) {
-        res.status(404).json({ message: 'Book not found' });
+      res.status(404).json({ message: 'Book not found' });
     } else {
-        res.json(book);
+      res.json(book);
     }
+  } catch (error) {
+    next(error);
+  }
 });
 
-app.put('/books/:id', async (req, res) => {
+app.put('/books/:id', async (req, res, next) => {
+  try {
     const { id } = req.params;
     const { title } = req.body;
     const book = await prisma.book.update({
-        where: { id: parseInt(id) },
-        data: { title },
+      where: { id: parseInt(id) },
+      data: { title },
     });
     res.json(book);
+  } catch (error) {
+    next(error);
+  }
 });
 
-app.delete('/books/:id', async (req, res) => {
+app.delete('/books/:id', async (req, res, next) => {
+  try {
     const { id } = req.params;
     await prisma.book.delete({ where: { id: parseInt(id) } });
     res.json({ message: 'Book deleted' });
+  } catch (error) {
+    next(error);
+  }
 });
 
 app.listen(port, () => {
-    console.log('Server is running on port ' + port);
+  console.log('Server is running on port ' + port);
 });
